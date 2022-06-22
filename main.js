b404e5f8a853ff5035088b0b2ed45a8b5e748eb8
MeenuMe <73883057+MeenuMe@users.noreply.github.com>
2022-06-22 17:30:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
-scoreLeftWrist = 0
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function setup() {
     canvas = createCanvas(600, 500);
@@ -18,6 +19,38 @@ function draw() {
 
     fill("#FF0000");
     stroke("#FF0000");
+
+    if(scoreRightWrist >0.2)
+    {
+        circle(rightWristX, rightWristY,20);
+
+        if(rightWristY >0 && rightWristY <= 100)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+        }
+        else if(rightWristY >100 && rightWristY <= 200)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1x";
+            song.rate(1);
+        }
+        else if(rightWristY >200 && rightWristY <= 300)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1.5x";
+            song.rate(1.5);
+        }
+        else if(rightWristY >300 && rightWristY <= 400) 
+        {
+            document.getElementById("speed").innerHTML = "SPeed = 2x";
+            song.rate(2);
+        }
+        else if(rightWristY >400 && rightWristY <= 500)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2x";
+            song.rate(2);
+        }
+    }
+
 
     if(scoreLeftWrist > 0.2)
     {
@@ -65,6 +98,9 @@ function gotPoses(results) {
     {
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         console.log("scoreLeftWrist = " + scoreLeftWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
